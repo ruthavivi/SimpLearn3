380453165e48731005812bf9c958809783b10ce5
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2022-12-29 22:52:17 +0200

    add game

--- app/src/main/java/com/example/simplearn/gamesFragment.java
+++ app/src/main/java/com/example/simplearn/gamesFragment.java
@@ -1,9 +1,12 @@
 package com.example.simplearn;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,34 @@ public class gamesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.gamesfragment,null);
+
+        View view=inflater.inflate(R.layout.gamesfragment,container, false);
+
+        Button button=(Button) view.findViewById(R.id.button2);
+        Button button3=(Button) view.findViewById(R.id.button3);
+        Button button4=(Button) view.findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.50languages.com/games/memo/he/es/1/1/"));
+                startActivity(browserIntent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.50languages.com/games/memo/he/es/1/1/"));
+                startActivity(browserIntent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.50languages.com/games/memo/he/es/1/1/"));
+                startActivity(browserIntent);
+            }
+        });
+        return view;
     }
 }
