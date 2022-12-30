66a624e16124bcdd39ffc9ac682d01106a89bbc9
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2022-12-30 09:18:35 +0200

    add article

--- app/src/main/java/com/example/simplearn/articleFragment.java
+++ app/src/main/java/com/example/simplearn/articleFragment.java
@@ -1,9 +1,12 @@
 package com.example.simplearn;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,87 @@ public class articleFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.articlefragment,null);
+        View view=inflater.inflate(R.layout.articlefragment,container, false);
+
+        Button button=(Button) view.findViewById(R.id.button2);
+        Button button3=(Button) view.findViewById(R.id.button3);
+        Button button4=(Button) view.findViewById(R.id.button4);
+        Button button5=(Button) view.findViewById(R.id.button5);
+        Button button6=(Button) view.findViewById(R.id.button6);
+        Button button7=(Button) view.findViewById(R.id.button7);
+        Button button8=(Button) view.findViewById(R.id.button8);
+        Button button9=(Button) view.findViewById(R.id.button9);
+
+
+
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtbDNHZ0pKVENnQVAB?hl=he&gl=IL&ceid=IL%3Ahe"));
+                startActivity(browserIntent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=en-US&gl=US&ceid=US:en"));
+                startActivity(browserIntent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=es-419&gl=AR&ceid=AR:es-419"));
+                startActivity(browserIntent);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=zh-CN&gl=CN&ceid=CN:zh-Hans"));
+                startActivity(browserIntent);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=ru&gl=RU&ceid=RU:ru"));
+                startActivity(browserIntent);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=ar&gl=AE&ceid=AE:ar"));
+                startActivity(browserIntent);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=it&gl=IT&ceid=IT:it"));
+                startActivity(browserIntent);
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/home?hl=fr&gl=FR&ceid=FR:fr"));
+                startActivity(browserIntent);
+            }
+        });
+        return view;
+
+
     }
-}
+    }
+
